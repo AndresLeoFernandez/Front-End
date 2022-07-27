75476f74a9283e06e6af05cbc236b63c0e0e579a
AndresLeoFernandez <104107412+AndresLeoFernandez@users.noreply.github.com>
2022-07-26 23:50:34 -0300

    agrego clase 4

---
+++ clase4/clase4-1.ts
@@ -0,0 +1,9 @@
+/*const cambiarColor = () => {
+  parrafo.classList.toggle("color-fondo-3");
+};*/
+const saludar = () => {
+  alert("Hola");
+};
+let boton = document.getElementById("boton");
+
+boton.addEventListener("click", saludar);
